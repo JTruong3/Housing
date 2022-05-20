704d6ad1b686a08b6bea7cee9e679764e7df9fe1
Jason Truong <truong3@ualberta.ca>
2022-05-20 10:47:43 -0400

    added price restriction to filter out rental properties

--- seleniumWebscrape.py
+++ seleniumWebscrape.py
@@ -22,16 +22,32 @@ headers = {
 }
 
 driver = webdriver.Chrome('/Users/jasontruong/Downloads/Learn/Rental/Housing/chromedriver')
-base_url = 'https://www.remax.ca/on/toronto-real-estate?pageNumber='
+base_url = 'https://www.remax.ca/on/markham-real-estate?pageNumber='
 test_list = []
-for m in range(1,2):
-    siteurl = base_url + str(m)
-    driver.get(siteurl)
+i = 0
+j = 1
+while len(test_list) <= 15:
 
-    websites = driver.find_elements_by_xpath('//a[@class="listing-card_listingCard__G6M8g"]')
+    for m in range(i,j):
+        siteurl = base_url + str(m)
+        driver.get(siteurl)
 
-    for i in websites:
-        test_list.append(i.get_attribute("href"))
+        # List of elements for websites
+        websites = driver.find_elements_by_xpath('//a[@class="listing-card_listingCard__G6M8g"]')
+    
+        # List of elements for prices
+        p_list = websites[0].find_elements_by_xpath('//h2[@class="listing-card_price__sL9TT"]')
+
+
+        for idx,i_sites in enumerate(websites):
+
+            h_price = int(p_list[idx].text.replace('$','').replace(',',''))
+            
+            # Removes rental properties
+            if h_price > 25000:
+                test_list.append(i_sites.get_attribute("href"))
+    i += 1
+    j += 1
 
 list_1 = []
 for i in tqdm(test_list):
@@ -42,8 +58,11 @@ for i in tqdm(test_list):
     
 dataTable = pd.DataFrame(list_1)
 dataTable["Estimated Monthly Mortgage Payment ($)"] = dataTable["Estimated Monthly Mortgage Payment ($)"].replace('Est. Payment: ', '', regex=True).replace(' monthly', '', regex=True)
+numCols = ['Price','Estimated Monthly Mortgage Payment ($)']
+dataTable[numCols] = dataTable[numCols].replace(',','',regex = True)
+dataTable[numCols] = dataTable[numCols].replace("\$",'',regex = True)
 dataTable['Link'] = test_list
-dataTable.to_csv("ProjectRental_Toronto.csv")
+dataTable.to_csv("ProjectRental_may19m.csv")
 
 
 driver.close
