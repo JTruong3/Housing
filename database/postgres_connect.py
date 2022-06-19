3515af1f2c071088b9ea8207c283ad92382bf099
Jason Truong <truong3@ualberta.ca>
2022-06-19 12:12:51 -0400

    Fixed up database

--- database/postgres_connect.py
+++ database/postgres_connect.py
@@ -2,6 +2,14 @@ from datetime import date
 from re import ASCII
 import psycopg2
 import pandas as pd
+import json
+
+day_var = date.today().strftime('%Y-%m-%d')
+
+f = open('database/counter.json')
+counters = json.load(f)
+condo_key_counter = counters['condo_key_counter']
+house_key_counter = counters['house_key_counter']
 
 try:
     connection = psycopg2.connect(
@@ -27,42 +35,54 @@ except:
 #     );""")
 # cursor.fetchone()[0]
 
-create_table_query = '''CREATE TABLE HOUSE_DATA 
-                        (Address VARCHAR(255),
-                        Description VARCHAR(255),
-                        Laundry VARCHAR(8),
-                        Price INT,
-                        Beds VARCHAR(255),
-                        Baths VARCHAR(255),
-                        Date_Listed VARCHAR(255),
-                        Property_Type VARCHAR(255),
-                        Monthly_Mortage VARCHAR(255),
-                        Link VARCHAR(255));
-            '''
+# create_table_query = '''CREATE TABLE condo_townh_data 
+#                         (id SERIAL PRIMARY KEY,
+#                         Address VARCHAR(255),
+#                         Description VARCHAR(10000),
+#                         Laundry VARCHAR(8),
+#                         Price INT,
+#                         Beds VARCHAR(255),
+#                         Baths VARCHAR(255),
+#                         Date_Listed VARCHAR(255),
+#                         Monthly_Mortage FLOAT,
+#                         Link VARCHAR(255));
+#             '''
 
-cursor.execute(create_table_query)
+# cursor.execute(create_table_query)
+# connection.commit()
 connection.commit()
-
-house_list = pd.read_csv("sample.csv")
+house_list = pd.read_csv("house_data/HouseData_{}.csv".format(day_var))
 for ind in house_list.index:
-    insert_placeholder = 'INSERT INTO house_data VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);'
+    
     address = house_list['Address'][ind]
-    description = str(ind)
+    description = house_list['Description'][ind]
     laundry = house_list['Laundry'][ind]
     price = int(house_list['Price'][ind])
-    beds = house_list['Beds'][ind]
-    baths = house_list['Baths'][ind]
+    beds = str(house_list['Beds'][ind])
+    baths = str(house_list['Baths'][ind])
     date_listed = house_list['Date Listed'][ind]
-    property_type = house_list['Property Type'][ind]
     monthly_mortgage = house_list['Estimated Monthly Mortgage Payment ($)'][ind]
     lnk = house_list['Link'][ind]
-    cursor.execute(insert_placeholder, (address,description,laundry,price,beds,baths, date_listed,property_type,monthly_mortgage,lnk))
-
+    if "-" in house_list['Address'][ind]:
+        condo_key_counter += 1
+        insert_placeholder = 'INSERT INTO condo_townh_data VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);'
+        cursor.execute(insert_placeholder, (condo_key_counter,address,description,laundry,price,beds,baths,date_listed,monthly_mortgage,lnk))
+    
+    else:
+        house_key_counter += 1
+        insert_placeholder = 'INSERT INTO house_data VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);'
+        cursor.execute(insert_placeholder, (house_key_counter,address,description,laundry,price,beds,baths,date_listed,monthly_mortgage,lnk))
+connection.commit()
 
 
 
 cursor.close()
 connection.close()
+
+newdict = {"condo_key_counter": condo_key_counter,"house_key_counter": house_key_counter}
+with open("database/counter.json", "w") as jsonFile:
+    json.dump(newdict, jsonFile)
+
 # ck_table = "SELECT * FROM pg_catalog.pg_tables;"
 #cursor.execute(ck_table)
 
